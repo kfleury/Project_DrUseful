a3916ecfa151af1d047cdd0ad2208053e9109db8
Vasek Tommy <tom.chauveau@epitech.eu>
2020-03-06 18:05:49 +0100

    feat: add all modules of the db

--- src/database/DbModule.js
+++ src/database/DbModule.js
@@ -4,6 +4,7 @@ const seq = new Sequelize({
     host: 'database.sqlite'
 });
 
+// ! Creation of drug Model in DB
 // ? List of content
 const texts = [
     'description',
@@ -11,7 +12,6 @@ const texts = [
     'usage',
     'danger',
 ];
-
 // ? Content of drug model
 const drugModel = {
     id: {
@@ -21,13 +21,12 @@ const drugModel = {
         primaryKey: true
     },
     name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
     },
     type: DataTypes.ENUM(['soft', 'hard'])
 };
-
 // ? Affect attributes to texts and put it in model drugInfo
 texts.forEach(text => {
     drugModel[text] = {
@@ -35,32 +34,94 @@ texts.forEach(text => {
         allowNull: true
     };
 });
-
 const drug = seq.define('drug', drugModel);
+
+// ! Creation of user model in DB
+const user = seq.define('user', {
+    id: {
+        type: DataTypes.UUIDV4,
+        allowNull: true,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    bio: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    perm: {
+        type: DataTypes.ENUM(['admin', 'user']),
+        allowNull: false
+    }
+});
+
+// ! Creation of effect model in DB
+const effect = seq.define('effect', {
+    id: {
+        type: DataTypes.UUIDV4,
+        allowNull: true,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
+    },
+    duration: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    power: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    type: {
+        type: DataTypes.ENUM(['chill', 'excitement', 'hallucination', 'euphoria']),
+        allowNull: false
+    }
+});
+
+// ! Creation of drugEffect associative model in DB
+const drugEffect = seq.define('drugEffect', {
+    drogueId: {
+        type: DataTypes.UUIDV4,
+        unique: true,
+        foreignKey: 'drug'
+    },
+    userId: {
+        type: DataTypes.UUIDV4,
+        unique: true,
+        foreignKey: 'user'
+    },
+
+});
+
+// ! Creation of userDrogueFav associative model in DB
+const userDrogueFav = seq.define('userDrogueFav', {
+    drogueId: {
+        type: DataTypes.UUIDV4,
+        unique: true,
+        foreignKey: 'drug'
+    },
+    userId: {
+        type: DataTypes.UUIDV4,
+        unique: true,
+        foreignKey: 'user'
+    },
+
+});
 
 async function initDatabase() {
     await seq.sync();
     console.log('Server Load');
 }
 
-async function testmyDb() {
-    await initDatabase();
-    await drug.create({
-        name: 'Cocaine',
-        description: 'test',
-        usage: 'con',
-        danger: 'danger',
-        type: 'hard'})
-        .then(e => console.log('test'))
-        .catch(e => console.log(e));
-    await drug.findAll()
-        .then(data => console.log(JSON.stringify(data, null, 4)))
-        .catch(e => console.log(e));
-}
-
-testmyDb();
 // ! Export
 module.exports = {
-    initDatabase, drug,
+    initDatabase, drug, user, effect, userDrogueFav, drugEffect,
 };
 
