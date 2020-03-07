9d90ce51644e341f8ec02008cd877d9d4b68520f
Marzocchi <olivier.marzocchi@epitech.eu>
2020-03-07 14:23:10 +0100

    [ADD]modules etcs

---
+++ my-app/build/precache-manifest.6bbae48446bf8f9f205ec6ced80a5d79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90fb1b10e753bb4fef37c788c0f5a8f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcafbf0abb3bd4777d1e",
+    "url": "/static/css/main.f8535632.chunk.css"
+  },
+  {
+    "revision": "c8ef56bb6934c23baf95",
+    "url": "/static/js/2.b220978e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.b220978e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcafbf0abb3bd4777d1e",
+    "url": "/static/js/main.5c407be2.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
